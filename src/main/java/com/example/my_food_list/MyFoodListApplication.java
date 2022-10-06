e22446ba4db52d184e66c004b2c6b719858689ef
ap3334 <chwil2989@naver.com>
2022-10-06 20:58:06 +0900

    초기 세팅

---
+++ src/main/java/com/example/my_food_list/MyFoodListApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_food_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFoodListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFoodListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/my_food_list/MyFoodListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_food_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFoodListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
